5f58102339973ec71016166af49c2bbe5fec2f5a
kalyani-Bhimavarapu <kalyanibhimavarapu5@gmail.com>
2021-02-25 00:36:10 +0530

    Connected the app with db

--- app.js
+++ app.js
@@ -1,18 +1,53 @@
-const express = require("express");
+const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
 const app = express();
 
+// Passport Config
+//require('./config/passport')(passport);
 
-//EJS
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-//Routes
-app.use('/', require('./routes/index'));
-app.use('/users', require('./routes/users'));
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 
-const port = process.env.PORT || "5050";
-  app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
-  });
+
+// Connect flash
+app.use(flash());
+
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+dbPassword = 'mongodb+srv://admin5:admin5@cluster0.uoe7c.mongodb.net/test?retryWrites=true&w=majority';
+module.exports = {
+    mongoURI: dbPassword
+};
